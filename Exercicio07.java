ad5d4acf67683d171a6f80ef374a041f3aa18082
Gabriel-Laurino <gabriel_laurino@outlook.com>
2024-03-22 17:09:12 -0300

    Exercício07

---
+++ Exercicio07.java
@@ -0,0 +1,8 @@
+public class Exercicio07 {
+    public static void executar() {
+        double custoFabrica = Prompt.lerDecimal("Digite o custo de fábrica do carro:");
+        
+        Carro carro = new Carro(custoFabrica);
+        carro.imprimirCustoConsumidor();
+    }
+}
