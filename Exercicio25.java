1d0d7d1f92f76745b7c10a29c7c4a0d36d8a4f24
gabriel-laurino <gabriel_laurino@outlook.com>
2024-03-22 20:37:28 -0300

    Exercício25

--- Exercicio25.java
+++ Exercicio25.java
@@ -1,3 +1,5 @@
+import util.Prompt;
+
 public class Exercicio25 {
     public static void executar() {
         Prompt.imprimir("\n**O EXERCÍCIO 25 FOI IMPLEMENTADO E RESOLVIDO NO EXERCÍCIO 24.**\n");
