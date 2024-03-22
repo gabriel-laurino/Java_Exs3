aa274e274e66af4dc7df03d83d112b82383848da
gabriel-laurino <gabriel_laurino@outlook.com>
2024-03-22 20:37:01 -0300

    Exercício05

--- Exercicio05.java
+++ Exercicio05.java
@@ -1,3 +1,6 @@
+import classes.Compra;
+import util.Prompt;
+
 public class Exercicio05 {
     public static void executar() {
         double valorCompra = Prompt.lerDecimal("Digite o valor da compra:");
