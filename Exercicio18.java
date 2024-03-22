b2e90b2ee8f44cdbcac4e13e8528520b51cb5665
Gabriel-Laurino <gabriel_laurino@outlook.com>
2024-03-22 17:09:13 -0300

    Exercício18

---
+++ Exercicio18.java
@@ -0,0 +1,11 @@
+public class Exercicio18 {
+    public static void executar() {
+        String nome = Prompt.lerLinha("\nDigite o nome do funcionário:");
+        int idade = Prompt.lerInteiro("Digite a idade do funcionário:");
+        char sexo = Character.toUpperCase(Prompt.lerChar("Digite o sexo do funcionário (M/F):"));
+        double salarioFixo = Prompt.lerDecimal("Digite o salário fixo do funcionário:");
+
+        FuncionarioAbono funcionario = new FuncionarioAbono(nome, idade, sexo, salarioFixo);
+        funcionario.imprimirSalarioComAbono();
+    }
+}
