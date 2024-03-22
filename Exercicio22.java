c237cec3d136d3bd257c76badee1a3ea88ddc912
gabriel-laurino <gabriel_laurino@outlook.com>
2024-03-22 20:37:25 -0300

    Exercício22

--- Exercicio22.java
+++ Exercicio22.java
@@ -1,3 +1,6 @@
+import classes.ContaLuz;
+import util.Prompt;
+
 public class Exercicio22 {
     public static void executar() {
         int tipoCliente = Prompt.lerInteiro("\nDigite o tipo de cliente (1-Residência, 2-Comércio, 3-Indústria):");
