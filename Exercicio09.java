5e995641ff5534b31f38f92dd4312e15a40182bf
Gabriel-Laurino <gabriel_laurino@outlook.com>
2024-03-22 17:09:12 -0300

    Exercício09

---
+++ Exercicio09.java
@@ -0,0 +1,7 @@
+public class Exercicio09 {
+    public static void executar() {
+        IntervaloNumeros intervalo = new IntervaloNumeros(80);
+        intervalo.lerNumeros();
+        intervalo.imprimirNumerosNoIntervalo(10, 150);
+    }
+}
