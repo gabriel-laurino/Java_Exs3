b88206721bbd0f3bd436c5e4d52c2ecb2450a98e
Gabriel-Laurino <gabriel_laurino@outlook.com>
2024-03-22 17:09:14 -0300

    Exercício19

---
+++ Exercicio19.java
@@ -0,0 +1,10 @@
+public class Exercicio19 {
+    public static void executar() {
+        int ladoA = Prompt.lerInteiro("\nDigite o comprimento do lado A:");
+        int ladoB = Prompt.lerInteiro("Digite o comprimento do lado B:");
+        int ladoC = Prompt.lerInteiro("Digite o comprimento do lado C:");
+
+        Triangulo triangulo = new Triangulo(ladoA, ladoB, ladoC);
+        triangulo.classificarTriangulo();
+    }
+}
