dd07d7643a25d3032e1d9d374eb3f274d47f457a
Gabriel-Laurino <gabriel_laurino@outlook.com>
2024-03-22 17:09:11 -0300

    Exercício02

---
+++ Exercicio02.java
@@ -0,0 +1,9 @@
+public class Exercicio02 {
+    public static void executar() {
+        double distancia = Prompt.lerDecimal("Digite a distância total percorrida (em km):");
+        double combustivel = Prompt.lerDecimal("Digite o total de combustível gasto (em litros):");
+        
+        Automovel automovel = new Automovel(distancia, combustivel);
+        automovel.imprimirConsumoMedio();
+    }
+}
