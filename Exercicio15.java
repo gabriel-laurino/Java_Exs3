2f7f02f7db0871107e555c58a83bb6e5584cda32
Gabriel-Laurino <gabriel_laurino@outlook.com>
2024-03-22 17:09:13 -0300

    Exercício15

---
+++ Exercicio15.java
@@ -0,0 +1,21 @@
+public class Exercicio15 {
+    public static void executar() {
+        double totalDesconto = 0, totalPago = 0;
+        double valor;
+        do {
+            valor = Prompt.lerDecimal("\nDigite o valor do veículo (0 para sair):");
+            if (valor == 0) break;
+            char tipoCombustivel = Character.toUpperCase(Prompt.lerChar("Digite o tipo de combustível (A/G/D):"));
+
+            VeiculoComDesconto veiculo = new VeiculoComDesconto(valor, tipoCombustivel);
+            veiculo.imprimirValorComDesconto();
+            
+            double desconto = veiculo.calcularDesconto();
+            totalDesconto += desconto;
+            totalPago += (valor - desconto);
+        } while (valor != 0);
+
+        Prompt.imprimirf("\nTotal de desconto concedido: R$ %.2f\n", totalDesconto);
+        Prompt.imprimirf("Total pago pelos clientes: R$ %.2f\n", totalPago);
+    }
+}
