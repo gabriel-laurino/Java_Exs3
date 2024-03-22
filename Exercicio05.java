f59af6aa0e431fcc0d65ef31e429fe8e4ea790c8
Gabriel-Laurino <gabriel_laurino@outlook.com>
2024-03-22 17:09:11 -0300

    Exercício05

---
+++ Exercicio05.java
@@ -0,0 +1,7 @@
+public class Exercicio05 {
+    public static void executar() {
+        double valorCompra = Prompt.lerDecimal("Digite o valor da compra:");
+        Compra compra = new Compra(valorCompra);
+        compra.imprimirPrestacoes();
+    }
+}
