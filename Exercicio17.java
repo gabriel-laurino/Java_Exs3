41bc37b80c83e80d318dc86cecaf33731b445e9e
Gabriel-Laurino <gabriel_laurino@outlook.com>
2024-03-22 17:09:13 -0300

    Exercício17

---
+++ Exercicio17.java
@@ -0,0 +1,13 @@
+public class Exercicio17{
+    public static void executar() {
+        double aumentoTotal = 0;
+        for (int i = 0; i < 5; i++) {
+            String nome = Prompt.lerLinha("\nDigite o nome do funcionário " + (i + 1) + ":");
+            double salarioAtual = Prompt.lerDecimal("Digite o salário atual do funcionário:");
+            Funcionario funcionario = new Funcionario(nome, salarioAtual);
+            funcionario.imprimirInformacoesReajustadas();
+            aumentoTotal += funcionario.calcularReajuste();
+        }
+        Prompt.imprimirf("\nAumento total na folha de pagamento: R$ %.2f\n", aumentoTotal);
+    }
+}
